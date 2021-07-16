937dce47854782a7d65e9cf11395f8b7a9878d26
TinaBBB <tinashen.tianshu@gmail.com>
2021-07-16 17:26:20 -0400

    Update run_yelp.sh
    modifying time limit conditions

--- cluster_bash/yelp/final_performance/run_yelp.sh
+++ cluster_bash/yelp/final_performance/run_yelp.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part1.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part2.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part3.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part4.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part5.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part6.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part7.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part8.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part9.sh
-sbatch --nodes=1 --time=00:03:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part10.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part1.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part2.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part3.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part4.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part5.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part6.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part7.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part8.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part9.sh
+sbatch --nodes=1 --time=00:30:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=def-ssanner ce-vae-part10.sh
 
